4ada7d51ca92526404c252332ea34eda2e138887
kotov_nikita <css55@i.ua>
2016-05-21 23:30:31 +0300

    refactor

--- MyListImpl.java
+++ MyListImpl.java
@@ -1,118 +1,128 @@
 package ua.khpi.kotov.task02;
 
-public class MyListImpl implements MyList{
-	
+public class MyListImpl implements MyList {
+
 	@Override
 	public String toString() {
-		String out="";
-		for(int i=0;i<mas.length;i++){
-			out+="["+mas[i]+"]";
+		String out = "";
+		for (int i = 0; i < mas.length; i++) {
+			out += "[" + mas[i] + "]";
 		}
 		return out;
 	}
 
+	MyListImpl() {
 
-	MyListImpl(){
-				
 	}
-	
+
 	Object[] mas;
-	int massize;
-	
+	int masSize;
 
 	@Override
 	public void add(Object e) {
-		massize++;
-		Object[] newmas= new Object[massize];
-		for(int i=0;i<massize-1;i++){
-		newmas[i]=mas[i];
+		masSize++;
+		Object[] newmas = new Object[masSize];
+		for (int i = 0; i < masSize - 1; i++) {
+			newmas[i] = mas[i];
 		}
-		newmas[massize-1]=e;
-		mas=newmas;
-		
+		newmas[masSize - 1] = e;
+		mas = newmas;
+
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		Object[] clearmas=new Object[0];
-		mas=clearmas;
-		massize=0;
+		Object[] clearmas = new Object[0];
+		mas = clearmas;
+		masSize = 0;
 	}
 
 	@Override
-	public boolean remove(Object o) { 
+	public boolean remove(Object o) {
 		// TODO Auto-generated method stub
-				int temp=-1;
-				boolean da=false;
-		for(int i=massize-1;i>-1;i--){
-			if (mas[i].equals(o)){temp=i;da=true;}
+		int temp = -1;
+		boolean da = false;
+		for (int i = masSize - 1; i > -1; i--) {
+			if (mas[i].equals(o)) {
+				temp = i;
+				da = true;
 			}
-		
-		int razmer=massize;
-		if (da){massize--;}
-		
-		Object[] newmas= new Object[massize];
-		int y=0;
-		for(int i=0;i<razmer;i++){
-			newmas[y]=mas[i];y++;
-			if(i==temp){y--;}
 		}
-		mas=newmas;
+
+		int razmer = masSize;
+		if (da) {
+			masSize--;
+		}
+
+		Object[] newmas = new Object[masSize];
+		int y = 0;
+		for (int i = 0; i < razmer; i++) {
+			newmas[y] = mas[i];
+			y++;
+			if (i == temp) {
+				y--;
+			}
+		}
+		mas = newmas;
 		return false;
 	}
 
 	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
-				
+
 		return mas;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return massize;
+		return masSize;
 	}
 
 	@Override
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub
-		boolean da=false;
-		for(int i=massize-1;i>-1;i--){
-			if (mas[i].equals(o)){da=true;}
+		boolean isContains = false;
+		for (int i = masSize - 1; i > -1; i--) {
+			if (mas[i].equals(o)) {
+				isContains = true;
 			}
-		return da;
+		}
+		return isContains;
 	}
 
 	@Override
 	public boolean containsAll(MyList c) {
 		// TODO Auto-generated method stub
-		
-		Object[] masc=c.toArray();
-		
-		//if(masc.length>mas.length){return false;}
-				
-		
-		boolean da=false;
-		
-		int temp=0;
-		int value=0;
-		
-		
-		for(int i=0;i<masc.length;i++){
-			
-			for(int y=0;y<mas.length;y++){
-				if(mas[y].equals(masc[i])){
-					da=true;
+
+		Object[] masc = c.toArray();
+
+		// if(masc.length>mas.length){return false;}
+
+		boolean isContains = false;
+
+		int temp = 0;
+		int value = 0;
+
+		for (int i = 0; i < masc.length; i++) {
+
+			for (int y = 0; y < mas.length; y++) {
+				if (mas[y].equals(masc[i])) {
+					isContains = true;
 					value++;
-					break;}
+					break;
+				}
 			}
-			if(value==temp){return false;}
-			else{temp++;}
+			if (value == temp) {
+				return false;
+			} else {
+				temp++;
+			}
 		}
 
-		return da;
+		return isContains;
 	}
 
 }
